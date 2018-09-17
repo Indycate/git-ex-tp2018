15253797c63cf7ffa00f39d75406587cd0cbd459
gruppe14 <gruppe14@wks05.isf.cs.tu-bs.de>
2018-09-17 12:33:14 +0200

    multiselect und annoying message gone

--- HelloTeamproject/src/application/GuiList.java
+++ HelloTeamproject/src/application/GuiList.java
@@ -26,13 +26,13 @@ public class GuiList extends JPanel {
 		JScrollPane pane = new JScrollPane(list);
 
 		DefaultListSelectionModel m = new DefaultListSelectionModel();
-		m.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		m.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		m.setLeadAnchorNotificationEnabled(false);
 		list.setSelectionModel(m);
 
 		list.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
-				System.out.println("[Values changed]");
+				//System.out.println("[Values changed]");
 			}
 		});
 
